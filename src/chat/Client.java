b68172013f06bde36caf53cbabf78a1919d37766
David Kundrats <davidkundrats@gmail.com>
2023-08-21 23:36:23 -0400

    first commit implemented client.java

---
+++ src/chat/Client.java
@@ -0,0 +1,55 @@
+package chat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+	public static void main(String[] args) {
+		try { 
+
+			System.out.println("Client Started"); 
+			Socket socket = new Socket("localhost", 5900); 
+			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+			PrintWriter output = new PrintWriter(socket.getOutputStream()); 
+			BufferedReader incoming = new BufferedReader(new InputStreamReader(socket.getInputStream())); 
+
+			Thread receiveThread = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						String message;
+						while ((message = incoming.readLine()) != null) {
+							System.out.println("Server: " + message);
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			receiveThread.start();
+
+			String userMessage;
+			while((userMessage = input.readLine()) != null) { 
+				output.println(userMessage); 
+				if(userMessage.equals("/exit")) { 
+					break; 
+				}
+			}
+
+			input.close(); 
+			output.close(); 
+			incoming.close(); 
+			socket.close(); 
+
+		}
+		catch(Exception e) { // improve granularity
+			System.out.println(e.toString()); 
+		}
+
+
+	}	
+}
