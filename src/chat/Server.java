1addfea71be7c4cc440511806b30c651ad209461
David Kundrats <davidkundrats@gmail.com>
2023-08-25 00:32:44 -0400

    begin server implementation

--- src/chat/Server.java
+++ src/chat/Server.java
@@ -1,5 +1,31 @@
 package chat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
+	public static void main(String[] args) { 
+		try {
+			ServerSocket ss = new ServerSocket(5900);
+			Socket clientSocket = ss.accept(); 
+			BufferedReader in = new BufferedReader( 
+					new InputStreamReader(clientSocket.getInputStream())); 
+			String str = in.readLine(); 
+			PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
+			
+//			while{ define this loop
+//				
+//			}
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+	}
 }
