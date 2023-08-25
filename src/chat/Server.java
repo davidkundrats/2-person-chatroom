3858605b1b2613c2ffde5c556a4417d0d7df2f7d
David Kundrats <davidkundrats@gmail.com>
2023-08-25 13:28:17 -0400

    server implementation updated

--- src/chat/Server.java
+++ src/chat/Server.java
@@ -6,26 +6,69 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
 
+	public static final List <PrintWriter> clientWriters = new ArrayList<>(); 
+	
 	public static void main(String[] args) { 
 		try {
 			ServerSocket ss = new ServerSocket(5900);
+			System.out.println("Server has started");
+			while(true) { 
+				
 			Socket clientSocket = ss.accept(); 
-			BufferedReader in = new BufferedReader( 
-					new InputStreamReader(clientSocket.getInputStream())); 
-			String str = in.readLine(); 
-			PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
+			PrintWriter clientWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+			clientWriters.add(clientWriter);
 			
-//			while{ define this loop
-//				
-//			}
+			Thread clientThread = new Thread (new ClientHandler(clientSocket, clientWriter)); 
+			clientThread.start();
+			} 
+			
 		
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
+		
+	}
+	private static class ClientHandler implements Runnable {
+		private Socket socket;
+		private PrintWriter writer; 
+		
+		public ClientHandler(Socket clientSocket, PrintWriter clientWriter) { 
+			this.socket = clientSocket; 
+			this.writer = clientWriter;
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			try { 
+				BufferedReader in = new BufferedReader(
+						new InputStreamReader(socket.getInputStream())); 
+				String message;
+				while((message = in.readLine()) != null) { 
+					System.out.println(message); 
+					broadcast(message);
+				}
+				in.close(); 
+				socket.close(); 
+				clientWriters.remove(writer); 
+			}
+			catch (IOException e){ 
+				e.printStackTrace();
+			}
+			
+		}
+
+		private void broadcast(String message) {
+			for(PrintWriter writer : clientWriters) { 
+					writer.println(message); 
+			}
+			
+		}
 		
 	}
 }
